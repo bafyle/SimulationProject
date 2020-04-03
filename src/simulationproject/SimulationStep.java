1f524d38a822aca747d2c1ed7c2842cba61b2500
Andrew-PC <Andrew-PC@Andrew>
2020-04-03 18:36:27 +0200

    added Monto Carlo algorithm

---
+++ src/simulationproject/SimulationStep.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulationproject;
+
+
+public class SimulationStep
+{
+    public int uniqueNumber;
+    public double probabilty;
+    public double comulative;
+    public int rangeMin;
+    public int rangeMax;
+    public SimulationStep()
+    {
+        
+    }
+}
